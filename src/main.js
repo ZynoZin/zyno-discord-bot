15fa50fa70256c2eb369698707b2642052e0fad4
ZynoZin <zineddinelamziouik@gmail.com>
2021-03-31 21:35:30 +0100

    inital commit

---
+++ src/main.js
@@ -0,0 +1,36 @@
+require('dotenv').config();
+
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+const prefix = '-';
+
+const fs = require('fs');
+
+client.commands = new Discord.Collection();
+
+const commandFiles = fs.readdirSync('./src/commands/').filter(file => file.endsWith('.js'));
+for (const file of commandFiles) {
+    const command = require(`./commands/${file}`);
+
+    client.commands.set(command.name, command);
+}
+
+
+client.once('ready', () => {
+    console.log('Codelyon is online!');
+});
+
+client.on('message', message => {
+    if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+    const args = message.content.slice(prefix.length).split(/ +/);
+    const command = args.shift().toLowerCase();
+
+    if (client.commands.get(command) != null) {
+        client.commands.get(command).execute(message, args);
+    }
+});
+
+client.login(process.env.BOT_TOKEN);
+
